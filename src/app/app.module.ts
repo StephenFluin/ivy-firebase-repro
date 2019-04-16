4e7846c72960ba45eaf85be02f163888405d1b07
Stephen Fluin <stephen.fluin@gmail.com>
2019-04-16 13:15:52 -0700

    build simple firebase app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBrWJx91j512T3q6AaTGNxu_3fq47bYhfg",
+      authDomain: "devfestmn.firebaseapp.com",
+      databaseURL: "https://devfestmn.firebaseio.com",
+      projectId: "firebase-devfestmn",
+      storageBucket: "firebase-devfestmn.appspot.com",
+      messagingSenderId: "27347020178"
+    }),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
